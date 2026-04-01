5b449e7146019bbd0328fb7e04de6e3cdc652b75
JessyChen210 <c1907755919@gmail.com>
2026-04-01 15:08:23 +0800

    Add files via upload

---
+++ photomator.js
@@ -0,0 +1,31 @@
+/*******************************
+脚本功能：Photomator照片编辑-解锁订阅
+频道地址：https://t.me/iSharesubcribe
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body photomator.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header photomator.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
+let obj = {};
+
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+}else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  if(body && body.subscriber) {
+    const product_id = "pixelmator_photo_lifetime_v1";
+    const entitlement = "pixelmator_photo_pro_access";
+    let data = {"expires_date": "9999-01-01T00:00:00Z","original_purchase_date":"2021-01-01T00:00:00Z","purchase_date": "2021-01-01T00:00:00Z","ownership_type": "PURCHASED","store": "app_store"};
+    let subscriber = body.subscriber;
+    subscriber.entitlements[(entitlement)] = subscriber.subscriptions[(product_id)] = data;        
+    subscriber.entitlements[(entitlement)].product_identifier = product_id;   
+    obj.body = JSON.stringify(body);
+  } 
+}
+
+$done(obj);
